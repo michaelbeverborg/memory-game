6a5ead6d450e43d18a5960b1739daa919b300790
Michael <michaelbeverborg@hotmail.com>
2022-01-25 21:21:12 +0100

    #5

--- src/App.js
+++ src/App.js
@@ -14,6 +14,8 @@ const cardImages = [
 function App() {
   const [cards, setCards] = useState([]);
   const [turns, setTurns] = useState(0);
+  const [choiceOne, setChoiceOne] = useState(null);
+  const [choiceTwo, setChoiceTwo] = useState(null);
 
   // Shuffle cards
   const shuffleCards = () => {
@@ -25,6 +27,13 @@ function App() {
     setCards(shuffledCards);
     setTurns(0);
   };
+
+  // Handle a choice
+  const handleChoice = card => {
+    // if choiceOne is not null set the clicked card on Two, else on One
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
+  };
+
   console.log(cards, turns);
   return (
     <div className="App">
@@ -34,7 +43,8 @@ function App() {
         {/** Map through every card */}
 
         {cards.map(card => (
-          <SingleCard key={card.id} card={card} />
+          // Add card and handleChoice as props to SingleCard
+          <SingleCard key={card.id} card={card} handleChoice={handleChoice} />
         ))}
       </div>
     </div>
